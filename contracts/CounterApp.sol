dd0621a369413ebe39b9e54e3f24806ede39eca4
Deveshb15 <deveshb583@gmail.com>
2022-10-09 12:07:36 +0530

    feat: update app

--- contracts/CounterApp.sol
+++ contracts/CounterApp.sol
@@ -6,7 +6,18 @@ import "@openzeppelin/contracts/utils/Counters.sol";
 
 contract CounterApp is ERC721URIStorage {
 
+    // Versions of the app
+    using Counters for Counters.Counter;
+    Counters.Counter public versions;
+
+    // Owner of the app
     address public appOwner;
+    // Builds tokenURI
+    mapping(uint256 => string) public builds; 
+
+    // Events
+    // Mint event
+    event Minted(address indexed from, address indexed to, uint256 version);
 
     constructor(string memory _tokenURI) ERC721("CounterApp", "CAS") {
         appOwner = msg.sender;
@@ -14,10 +25,28 @@ contract CounterApp is ERC721URIStorage {
     }
 
     function mint(string memory _tokenURI) private returns(uint256) {
+        versions.increment();
         uint256 tokenId = 1;
+        uint256 currentVersion = versions.current();
         _mint(msg.sender, tokenId);
         _setTokenURI(tokenId, _tokenURI);
 
+        builds[currentVersion] = _tokenURI;
+
+        emit Minted(msg.sender, address(this), currentVersion);
+
         return tokenId;
+    }
+
+    function getPrevioudBuild(uint256 versionNumber) public view returns(string memory) {
+        return builds[versionNumber];
+    }
+
+    function updateApp(string memory _tokenURI) public {
+        require(msg.sender == appOwner, "Only owner can update app");
+        uint256 currentVersion = versions.current();
+        _setTokenURI(1, _tokenURI);
+        builds[currentVersion+1] = _tokenURI;
+        versions.increment();
     }
 }
